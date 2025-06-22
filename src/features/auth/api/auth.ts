b19f6f53b0dc7ec512fa8286ba755621ba62fbf6
maomao <meladryyy@gmail.com>
2025-06-23 00:33:09 +0800

    refactor: add return types to functions  and  in

--- src/features/auth/api/auth.ts
+++ src/features/auth/api/auth.ts
@@ -1,12 +1,16 @@
 import { getSupabaseClient } from "@/app/supabase";
+import { AuthError } from "@supabase/supabase-js";
 
-export async function signInUser(email: string, password: string) {
+export async function signInUser(
+  email: string,
+  password: string
+): Promise<AuthError | null> {
   const supabase = await getSupabaseClient();
   const { error } = await supabase.auth.signInWithPassword({ email, password });
   return error;
 }
 
-export async function signOutUser() {
+export async function signOutUser(): Promise<AuthError | null> {
   const supabase = await getSupabaseClient();
   const { error } = await supabase.auth.signOut();
   return error;
