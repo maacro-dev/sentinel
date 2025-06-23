371e4c8327d9458e15b0525dd85e0fc5c7555244
maomao <meladryyy@gmail.com>
2025-06-23 10:05:33 +0800

    chore: remove unused file admin-dashboard.tsx

--- src/features/admin/admin-dashboard.tsx
+++
@@ -1,7 +0,0 @@
-import { PageDefinitionOnly } from "@/components/page-definition-only";
-
-const AdminDashboard = () => {
-  return <PageDefinitionOnly label="system admin" />;
-};
-
-export default AdminDashboard;
