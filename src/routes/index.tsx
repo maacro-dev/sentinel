fe8f34bcc49cf753836edf75c0815cce7c02e3fc
mawmao <meladryyy@gmail.com>
2025-06-30 06:43:03 +0800

    Run Biome formatter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,15 +8,17 @@ export const Route = createFileRoute("/")({
 function App() {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen ">
-      <div className="flex flex-col items-start justify-start gap-6">
-        <div>
-          <p className="text-sm text-muted-foreground">branch</p>
-          <h1 className="text-3xl font-bold">prototype/secondary</h1>
-        </div>
-        <Link to="/login">
-        <Button variant="default" size="lg">Login</Button>
-        </Link>
-      </div>
+			<div className="flex flex-col items-start justify-start gap-6">
+				<div>
+					<p className="text-sm text-muted-foreground">branch</p>
+					<h1 className="text-3xl font-bold">prototype/secondary</h1>
+				</div>
+				<Link to="/login">
+					<Button variant="default" size="lg">
+						Login
+					</Button>
+				</Link>
+			</div>
 		</div>
 	);
 }
