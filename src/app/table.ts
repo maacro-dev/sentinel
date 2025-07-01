2f9c16939caff0fb41c63a26ace46bc5120f27c7
mawmao <meladryyy@gmail.com>
2025-07-01 22:34:39 +0800

    chore: disable eslint on module declaration for type-safety

--- src/app/table.ts
+++ src/app/table.ts
@@ -1,6 +1,7 @@
 import { RowData } from "@tanstack/react-table";
 
 declare module "@tanstack/react-table" {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ColumnMeta<TData extends RowData, TValue> {
     style?: {
       textAlign: "left" | "center" | "right";
