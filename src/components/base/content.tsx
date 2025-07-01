2e5cc04aca0c4cfc83fec37ccef33a0a7188900a
mawmao <meladryyy@gmail.com>
2025-07-02 04:26:09 +0800

    refactor: rename HumayBaseContent to BaseContent

--- src/components/base/content.tsx
+++ src/components/base/content.tsx
@@ -1,6 +1,7 @@
-type HumayBaseContentProps = {
+type BaseContentProps = {
   children: React.ReactNode;
 };
-export const HumayBaseContent = ({ children }: HumayBaseContentProps) => {
+
+export const BaseContent = ({ children }: BaseContentProps) => {
   return <div className="flex h-full flex-1 flex-col gap-4 p-4 pt-0">{children}</div>;
 };
