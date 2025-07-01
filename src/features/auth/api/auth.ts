9574af6e5b103f63f568ccf6e6235d2dcc688d44
mawmao <meladryyy@gmail.com>
2025-07-01 22:42:00 +0800

    refactor: rename authentication functions for consistency with Supabase API

--- src/features/auth/api/auth.ts
+++ src/features/auth/api/auth.ts
@@ -1,15 +1,12 @@
 import { supabase } from "@/app/supabase";
 import { AuthError } from "@supabase/supabase-js";
 
-export async function signInUser(
-  email: string,
-  password: string
-): Promise<AuthError | null> {
+export async function supabaseSignIn(email: string,password: string): Promise<AuthError | null> {
   const { error } = await supabase.auth.signInWithPassword({ email, password });
   return error;
 }
 
-export async function signOutUser(): Promise<AuthError | null> {
+export async function supabaseSignOut(): Promise<AuthError | null> {
   const { error } = await supabase.auth.signOut();
   return error;
 }
