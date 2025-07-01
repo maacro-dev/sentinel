72413f141f6fcacc071b1a1663c1bfa228342b54
mawmao <meladryyy@gmail.com>
2025-07-01 22:53:56 +0800

    feat: add barrel file for src/utils

---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+export { hasValidStaticData } from "./route/has-valid-static-data";
+
+export { isRoleAllowed } from "./auth/is-role-allowed";
+
+export { getSidebarGroupRank } from "./sidebar/get-sidebar-rank";
+export { toSidebarItem } from "./sidebar/to-sidebar-item";
+export { groupSidebarItems } from "./sidebar/group-sidebar-items";
+
+export { getRoleLabel } from "./role/get-role-label";
