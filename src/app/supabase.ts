17cd60e3304fdc2a5a7481062f4749eb3993e951
mawmao <meladryyy@gmail.com>
2025-07-01 22:34:06 +0800

    refactor: use .env variables and remove hardcoded url strings

--- src/app/supabase.ts
+++ src/app/supabase.ts
@@ -1,16 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "./database.types";
 
-const supabaseUrl =
-  import.meta.env.MODE === "development"
-    ? "http://127.0.0.1:54321"
-    : "https://lmhkgxzfqzryocbjqcfi.supabase.co";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabaseKey =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_SUPABASE_KEY_DEV
-    : import.meta.env.VITE_SUPABASE_KEY;
-
-const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
