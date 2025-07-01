e68b6751ab4bf849355edd9c443f0e2dc55ec4e2
mawmao <meladryyy@gmail.com>
2025-07-02 02:08:28 +0800

    refactor: update user schema to include user ID validation

--- src/lib/schemas/user.ts
+++ src/lib/schemas/user.ts
@@ -8,17 +8,20 @@ import {
   uuidv4,
   nullable,
   array,
+  regex,
 } from "zod/v4-mini";
 
 export const userRoleSchema = enum_(["data_collector", "data_manager", "admin", "*"] as const);
 
 export const userStatusSchema = enum_(["active", "inactive", "disabled"]);
 
-export const userDetailSchema = object({
+export const userIdSchema = string().check(regex(/^DA\d{4}$/));
+
+export const userSchema = object({
   auth_id: uuidv4(),
+  user_id: userIdSchema,
   first_name: string().check(minLength(1)),
   last_name: string().check(minLength(1)),
-  username: string().check(minLength(1)),
   status: userStatusSchema,
   role: userRoleSchema,
   email: email(),
@@ -27,27 +30,18 @@ export const userDetailSchema = object({
   updated_at: iso.datetime({ offset: true }),
 });
 
-export const userSummarySchema = object({
-  auth_id: uuidv4(),
-  full_name: string(),
-  status: userStatusSchema,
-  role: userRoleSchema,
-  email: email(),
-  last_sign_in_at: nullable(iso.datetime({ offset: true })),
-});
-
-export const userSummarySchemaArray = array(userSummarySchema);
+export const usersSchema = array(userSchema);
 
 export const userCredentialsSchema = object({
-  user_id: string().check(minLength(1, "National ID is required to log in")),
+  user_id: string().check(minLength(1, "User ID is required to log in")),
   password: string().check(minLength(1, "Password is required to log in")),
 });
 
-export const userCreateSchema = object({
-  first_name: string().check(minLength(1)),
-  last_name: string().check(minLength(1)),
-  username: string().check(minLength(1)),
-  status: userStatusSchema,
-  role: userRoleSchema,
-  email: email(),
-});
+// export const userCreateSchema = object({
+//   first_name: string().check(minLength(1)),
+//   last_name: string().check(minLength(1)),
+//   username: string().check(minLength(1)),
+//   status: userStatusSchema,
+//   role: userRoleSchema,
+//   email: email(),
+// });
