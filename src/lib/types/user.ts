5a6f1e8ad92e35fc1b0bf44f502da4a87760e558
mawmao <meladryyy@gmail.com>
2025-07-01 22:53:41 +0800

    refactor: enhance sidebar and user types

--- src/lib/types/user.ts
+++ src/lib/types/user.ts
@@ -1,18 +1,31 @@
 import type { infer as Infer } from "zod/v4-mini";
 import type {
-  userRoleSchema,
   userStatusSchema,
-  userDetailSchema,
   userSummarySchema,
   userCredentialsSchema,
   userCreateSchema,
   userSummarySchemaArray,
+  userRoleSchema,
 } from "@/lib/schemas/user";
 
-export type Role = Infer<typeof userRoleSchema>;
-export type User = Infer<typeof userDetailSchema>;
 export type UserSummary = Infer<typeof userSummarySchema>;
 export type UserSummaryArray = Infer<typeof userSummarySchemaArray>;
 export type UserStatus = Infer<typeof userStatusSchema>;
 export type UserCredentials = Infer<typeof userCredentialsSchema>;
 export type UserCreate = Infer<typeof userCreateSchema>;
+
+
+export type Role = Infer<typeof userRoleSchema>;
+export type User = {
+  auth_id: string
+  created_at: string
+  date_of_birth: string
+  email: string
+  first_name: string
+  last_name: string
+  last_sign_in_at: string
+  user_id: string
+  role: Role
+  status: UserStatus
+  updated_at: string
+}
