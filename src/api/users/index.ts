52dafcf7b7e586240714c08f891dac5de39f9192
mawmao <meladryyy@gmail.com>
2025-07-03 01:56:18 +0800

    feat: export generateUserId function

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -1,4 +1,9 @@
 export { getAllUsers } from "./get-all-users";
 export { getUserById } from "./get-user-by-id";
+export { getNextUserId } from "./generate-user-id";
 
-export { usersQueryOptions, userQueryOptions } from "./queries";
+export {
+    usersQueryOptions,
+    userQueryOptions,
+    generateUserIdQueryOptions
+} from "./queries";
