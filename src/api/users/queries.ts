e7f6d2f6c08664980b2803c2ce3a0e90937e99e1
mawmao <meladryyy@gmail.com>
2025-07-03 01:57:23 +0800

    feat: create queryOptions for getting next user id api call

--- src/api/users/queries.ts
+++ src/api/users/queries.ts
@@ -1,12 +1,23 @@
-import { getAllUsers, getUserById } from "./";
+import { queryOptions } from "@tanstack/react-query";
+import { getAllUsers, getUserById, getNextUserId } from "./";
 
-export const usersQueryOptions = {
-  queryKey: ["users"] as const,
-  queryFn: getAllUsers,
-};
+export const usersQueryOptions = () => 
+  queryOptions({
+    queryKey: ["users"] as const,
+    queryFn: getAllUsers,
+    staleTime: 1000 * 60 * 10, // 5 minutes
+  })
 
-export const userQueryOptions = (userId: string) => ({
-  queryKey: ["user", userId] as const,
-  queryFn: () => getUserById(userId),
-  enabled: Boolean(userId),
-});
+export const generateUserIdQueryOptions = () => 
+  queryOptions({
+    queryKey: ["generate-user-id"] as const,
+    queryFn: getNextUserId,
+  })
+
+export const userQueryOptions = (userId: string) => 
+  queryOptions({
+    queryKey: ["user", userId] as const,
+    queryFn: () => getUserById(userId),
+    enabled: Boolean(userId),
+  })
+
