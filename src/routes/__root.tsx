8e95235005ad53bbb758671443ec40347492bd49
mawmao <meladryyy@gmail.com>
2025-07-03 01:54:21 +0800

    feat: add React Query Devtools to root component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,11 @@
 import { HeadContent, Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { RouterContext } from "@/app/router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@/components/ui/sonner";
 
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootComponent,
-  errorComponent: ({ error }) => <div>{error.message}</div>
 });
 
 function RootComponent() {
@@ -14,6 +14,7 @@ function RootComponent() {
       <HeadContent />
       <Outlet />
       <Toaster />
+      <ReactQueryDevtools buttonPosition="top-right" />
       <TanStackRouterDevtools position="bottom-right" />
     </>
   );
