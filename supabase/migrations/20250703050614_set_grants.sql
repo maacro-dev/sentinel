7746fdbbffa176d18e8c529df04b635b86308169
mawmao <meladryyy@gmail.com>
2025-07-03 16:51:40 +0800

    feat: update user permissions and policies to allow authenticated users to insert and access their own data

--- supabase/migrations/20250703050614_set_grants.sql
+++ supabase/migrations/20250703050614_set_grants.sql
@@ -5,6 +5,6 @@ GRANT SELECT ON public.users TO public;
 GRANT SELECT ON public.roles TO public;
 
 GRANT SELECT ON public.user_profiles TO authenticated;
-GRANT SELECT ON public.users TO authenticated;
+GRANT SELECT, INSERT ON public.users TO authenticated;
 GRANT SELECT ON public.roles TO authenticated;
 
--- supabase/migrations/20250703050619_set_policies.sql
+++ supabase/migrations/20250703050619_set_policies.sql
@@ -7,11 +7,19 @@ CREATE POLICY "Users can access roles"
   USING (true);
 
 ALTER TABLE public.users ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "Users can access their own data"
+CREATE POLICY "All users can access their own data"
   ON public.users
   AS PERMISSIVE
   FOR SELECT
   TO public
   USING (true);
+
+CREATE POLICY "Enable insert for authenticated users only"
+  ON public.users
+  AS PERMISSIVE
+  FOR INSERT
+  TO authenticated
+  WITH CHECK (true);
+
 
 
