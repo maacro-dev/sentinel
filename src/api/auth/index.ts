4067438a4bd31cdbb6495d8e43e0ea7351fdd3bc
mawmao <meladryyy@gmail.com>
2025-07-03 12:46:46 +0800

    feat: add supabaseSignUp function definition and update barrel file

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,2 +1,3 @@
 export { supabaseSignIn } from "./sign-in";
 export { supabaseSignOut } from "./sign-out";
+export { supabaseSignUp } from "./sign-up";
---
+++ src/api/auth/sign-up.ts
@@ -0,0 +1,7 @@
+import {UserCreate} from "@/lib/types";
+// import {supabase} from "@/app/supabase.ts";
+
+
+export async function supabaseSignUp(userCreate: UserCreate) {
+  console.log("supabaseSignUp", userCreate)
+}
