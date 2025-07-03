2b347fd5e90748289e44aec37985bb6af435b54c
mawmao <meladryyy@gmail.com>
2025-07-03 13:20:31 +0800

    feat: configure Supabase client for development and production environments

--- src/app/supabase.ts
+++ src/app/supabase.ts
@@ -1,9 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "@/app/supabase.types";
+import { IS_DEV } from "@/lib/utils";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseDevUrl = import.meta.env.VITE_SUPABASE_DEV_URL;
+const supabaseDevKey = import.meta.env.VITE_SUPABASE_DEV_ANON_KEY;
 
-const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+const supabaseProdUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseProdKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = IS_DEV ? 
+  createClient<Database>(supabaseDevUrl, supabaseDevKey) : 
+  createClient<Database>(supabaseProdUrl, supabaseProdKey);
 
 export { supabase };
