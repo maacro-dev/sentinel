6ba01a81e847258a9db31b6438e1894d35f19ea3
mawmao <meladryyy@gmail.com>
2025-07-04 21:21:50 +0800

    refactor: replace hacky session handling when creating user using edge functions (#26)
    Closes #26

--- src/api/users/create-user.ts
+++ src/api/users/create-user.ts
@@ -1,43 +1,7 @@
-import { ROLE_IDS } from "@/app/config";
-import { supabase } from "@/app/supabase";
-import {UserCreate} from "@/lib/types";
+import type { Result, UserCreate } from "@/lib/types";
+import { invokeFunction } from "@/utils";
 
-export async function createUser({
-  first_name,
-  last_name,
-  email,
-  role,
-  date_of_birth,
-}: UserCreate) {
-
-  // [hack]
-  const { data: currentSession } = await supabase.auth.getSession() 
-
-  const { data, error } = await supabase.auth.signUp({
-    email,
-    password: "123456", // TODO: randomize password
-  });
-
-  if (error || !data) {
-    throw new Error(`Failed to sign up auth.user: ${error?.message}`);
-  }
-
-  const { user } = data;
-
-  const { error: userError } = await supabase
-    .from("users")
-    .insert({
-      auth_id: user?.id,
-      first_name: first_name,
-      last_name: last_name,
-      role_id: ROLE_IDS[role],
-      date_of_birth,
-    });
-
-  if (userError) {
-    throw new Error(`Failed to create public.user: ${userError.message}`);
-  }
-
-  // [hack]
-  supabase.auth.setSession(currentSession.session!)
+// maybe make as mutation?
+export async function createUser(user: UserCreate): Promise<Result<void>> {
+  return await invokeFunction("create-user", user)
 }
