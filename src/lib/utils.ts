3ec6cec420d6e26b650d84d78eca9e70d498cf94
mawmao <meladryyy@gmail.com>
2025-07-04 22:29:11 +0800

    refactor: remove unused formatDate function from utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,22 +7,4 @@ export function cn(...inputs: ClassValue[]) {
 
 export const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
-export function formatDate(
-  date: Date,
-  locale: string = 'en-US',
-  options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }
-): string {
-
-  if (!(date instanceof Date) || isNaN(date.getTime())) {
-    throw new Error('Invalid Date object');
-  }
-
-  return new Intl.DateTimeFormat(locale, options).format(date);
-}
-
-
 export const IS_DEV = import.meta.env.DEV;
