41a542174f3b686beac60518af4e071937a5f1f6
mawmao <meladryyy@gmail.com>
2025-07-06 16:51:13 +0800

    refactor: add session checking and logging functionality in Root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,12 +3,28 @@ import { RouterContext } from "@/app/router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@/components/ui/sonner";
+import { useAuthStore } from "@/store/auth-store";
+import { logRender, logPreload, logDebugCheck, logDebugOk, logDebugError } from "@/utils/log";
 
 export const Route = createRootRouteWithContext<RouterContext>()({
+  beforeLoad: async () => {
+    logPreload("Root Route");
+    logDebugCheck("Check if session is already checked");
+    const sessionChecked = useAuthStore.getState().sessionChecked;
+    if (sessionChecked) {
+      logDebugOk("Session is already checked");
+      return;
+    }
+    logDebugError("Session is not checked");
+    logDebugCheck("Checking session...");
+    await useAuthStore.getState().checkSession();
+    logDebugOk("Session checked");
+  },
   component: RootComponent,
 });
 
 function RootComponent() {
+  logRender("Root Route");
   return (
     <>
       <HeadContent />
