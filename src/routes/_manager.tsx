8b330ddaf3467c16ecac6562416fb1f4c0c947f6
mawmao <meladryyy@gmail.com>
2025-07-06 16:51:58 +0800

    refactor: use auth store in checking auth before load

--- src/routes/_manager.tsx
+++ src/routes/_manager.tsx
@@ -1,11 +1,21 @@
 import { BaseLayout } from "@/components/layouts";
 import { BaseSidebar } from "@/components/layouts/base";
-import { protectRoute } from "@/features/auth/utils";
-import { Outlet, createFileRoute } from "@tanstack/react-router";
+import { useAuthStore } from "@/store/auth-store";
+import { log } from "@/utils";
+import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_manager")({
-  beforeLoad: ({ context }) => {
-    protectRoute(context, { allowedRoles: "data_manager" });
+  beforeLoad: async () => {
+    const user = useAuthStore.getState().user;
+    log("CHECK", `User: ${user ? "exists" : "does not exist"}`);
+    if (!user) {
+      log("ERROR", "User does not exist → redirecting to login");
+      throw redirect({ to: "/login" });
+    }
+    if (user.role !== "data_manager") {
+      log("ERROR", "User is not a data manager → redirecting to unauthorized");
+      throw redirect({ to: "/unauthorized" });
+    }
   },
   component: RouteComponent,
 });
