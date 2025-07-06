47b28ff85e9006688e6b829c0a096368e28df4a1
mawmao <meladryyy@gmail.com>
2025-07-06 16:46:38 +0800

    refactor: update export for getCurrentSession to use new module

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,3 +1,3 @@
 export { supabaseSignIn } from "./sign-in";
 export { supabaseSignOut } from "./sign-out";
-export { getCurrentSession } from "./session";
+export { getCurrentSession } from "./get-session";
