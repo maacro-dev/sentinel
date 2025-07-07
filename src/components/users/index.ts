af3e5231fdfbbd84340789afb27043c73c20054b
mawmao <meladryyy@gmail.com>
2025-07-07 19:50:22 +0800

    refactor: remove useUserColumns hook and replace DataTable with a simplified table structure in user management

--- src/components/users/index.ts
+++ src/components/users/index.ts
@@ -1,2 +1,1 @@
 export { AddUserForm } from "./user-create-dialog";
-export { useUserColumns } from "./user-table/user-table-columns";
--- src/routes/admin/user_management.tsx
+++ src/routes/admin/user_management.tsx
@@ -1,23 +1,27 @@
 import { createUser } from "@/api/users";
 import { showErrorToast, showSuccessToast } from "@/app/toast";
-import { DataTable } from "@/components/ui/data-table";
 import { Input } from "@/components/ui/input";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { AddUserForm } from "@/components/users/user-create-dialog";
-import { useUserColumns } from "@/components/users/user-table/user-table-columns";
 import { UserCreate } from "@/lib/types/user";
 import { usersQueryOptions } from "@/queries";
 import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { Search as SearchIcon, Users } from "lucide-react";
-import { logRender } from "chronicle-log";
+import { logLoad, logRender } from "chronicle-log";
+import { TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from "@/components/ui/table";
+import { UserActionsCell } from "@/components/users/user-table/cells/user-actions-cell";
+import { LastActiveCell, StatusCell } from "@/components/users/user-table/cells";
+import { ROLE_LABELS } from "@/app/config";
 
 export const Route = createFileRoute("/admin/user_management")({
   component: RouteComponent,
   head: () => ({
     meta: [{ title: "User Management | Humay" }],
   }),
-  loader: ({ context: { queryClient } }) => queryClient.ensureQueryData(usersQueryOptions()),
+  loader: ({ context: { queryClient } }) => {
+    logLoad("User Management Data");
+    queryClient.ensureQueryData(usersQueryOptions({ includeAdmin: false }));
+  },
   staticData: {
     routeFor: "admin",
     label: "User Management",
@@ -30,17 +34,15 @@ export const Route = createFileRoute("/admin/user_management")({
 function RouteComponent() {
   logRender("Admin User Management Route");
 
-  const { data: users } = useSuspenseQuery(usersQueryOptions())
+  const { data: users } = useSuspenseQuery(usersQueryOptions({ includeAdmin: false }))
   const queryClient = useQueryClient()
-
-  const columns = useUserColumns();
 
   const onSubmit = async (fields: UserCreate) => {
     const result = await createUser(fields)
     if (!result.ok) {
       showErrorToast("Error", result.error?.message)
     } else {
-      await queryClient.invalidateQueries(usersQueryOptions())
+      await queryClient.invalidateQueries(usersQueryOptions({ includeAdmin: false }))
       showSuccessToast("Success", "User created successfully")
     }
   };
@@ -56,66 +58,42 @@ function RouteComponent() {
         </div>
         <AddUserForm onSubmit={onSubmit} />
       </div>
+      <div className="flex flex-col gap-4">
       <div>
         <div className="w-72 flex items-center gap-2">
           <SearchIcon className="text-muted-foreground" />
           <Input placeholder="Search" />
         </div>
       </div>
-      <Tabs defaultValue="all">
-        <TabsList className="p-1">
-          <TabsTrigger
-            className="px-2 py-1 min-w-16 text-muted-foreground data-[state=active]:text-primary data-[state=active]:shadow-none"
-            value="all"
-          >
-            All
-          </TabsTrigger>
-          <TabsTrigger
-            className="px-4 py-1 text-muted-foreground data-[state=active]:text-primary data-[state=active]:shadow-none"
-            value="data_collectors"
-          >
-            Data Collectors
-          </TabsTrigger>
-          <TabsTrigger
-            className="px-4 py-1 text-muted-foreground data-[state=active]:text-primary data-[state=active]:shadow-none"
-            value="data_managers"
-          >
-            Data Managers
-          </TabsTrigger>
-          <TabsTrigger
-            className="px-4 py-1 text-muted-foreground data-[state=active]:text-primary data-[state=active]:shadow-none"
-            value="admins"
-          >
-            Admins
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="all">
-          <div className="flex-1 container w-full h-full">
-            <DataTable columns={columns} data={users.filter((u) => u.role !== "admin")} />
-          </div>
-        </TabsContent>
-        <TabsContent value="data_collectors">
-          <div className="flex-1 container w-full h-full">
-            <DataTable
-              columns={columns}
-              data={users.filter((u) => u.role === "data_collector")}
-            />
-          </div>
-        </TabsContent>
-        <TabsContent value="data_managers">
-          <div className="flex-1 container w-full h-full">
-            <DataTable
-              columns={columns}
-              data={users.filter((u) => u.role === "data_manager")}
-            />
-          </div>
-        </TabsContent>
-        <TabsContent value="admins">
-          <div className="flex-1 container w-full h-full">
-            <DataTable columns={columns} data={users.filter((u) => u.role === "admin")} />
-          </div>
-        </TabsContent>
-      </Tabs>
+        <h2 className="text-2xl font-semibold">Data Collectors</h2>
+        <Table className="flex-1 w-full h-full">
+          <TableHeader>
+              <TableRow>
+                <TableHead className="">Name</TableHead>
+                <TableHead className="">Email</TableHead>
+                <TableHead className="">Role</TableHead>
+                <TableHead className="">Status</TableHead>
+                <TableHead className="">Last Sign In</TableHead>
+                <TableHead className="">Actions</TableHead>
+              </TableRow>
+          </TableHeader>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow 
+                key={user.id} 
+                className={`hover:bg-muted/50 ${user.role === "data_manager" && "bg-muted"}`}
+                >
+                <TableCell>{user.first_name} {user.last_name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{ROLE_LABELS[user.role]}</TableCell>
+                <TableCell><StatusCell status={user.status} /></TableCell>
+                <TableCell><LastActiveCell lastActive={user.last_sign_in_at} /></TableCell>
+                <TableCell><UserActionsCell user={user} /></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
