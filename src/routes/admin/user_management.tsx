aae7d41792396c5adb71d1c714aa4e3a5d6f4ba8
mawmao <meladryyy@gmail.com>
2025-07-10 14:32:15 +0800

    refactor: enhance user management interface by integrating CreateUserDialog and SearchBar components, and optimizing data fetching with Suspense

--- src/routes/admin/user_management.tsx
+++ src/routes/admin/user_management.tsx
@@ -1,17 +1,13 @@
-import { createUser } from "@/api/users";
-import { showErrorToast, showSuccessToast } from "@/app/toast";
-import { Input } from "@/components/ui/input";
-import { AddUserForm } from "@/components/users/user-create-dialog";
+import { CreateUserDialog } from "@/components/users/user-create-dialog";
 import { UserCreate } from "@/lib/types/user";
-import { usersQueryOptions } from "@/queries";
-import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { Search as SearchIcon, Users } from "lucide-react";
+import { Users } from "lucide-react";
 import { logLoad, logRender } from "chronicle-log";
-import { TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from "@/components/ui/table";
-import { UserActionsCell } from "@/components/users/user-table/cells/user-actions-cell";
-import { LastActiveCell, StatusCell } from "@/components/users/user-table/cells";
-import { ROLE_LABELS } from "@/app/config";
+import { createUserMutation } from "@/mutations";
+import { SearchBar } from "@/components/search-bar";
+import { UsersTableSkeleton, UsersTableSuspense } from "@/components/users/users-table";
+import { Suspense } from "react";
+import { usersQueryOptions } from "@/queries";
 
 export const Route = createFileRoute("/admin/user_management")({
   component: RouteComponent,
@@ -21,6 +17,7 @@ export const Route = createFileRoute("/admin/user_management")({
   loader: ({ context: { queryClient } }) => {
     logLoad("User Management Data");
     queryClient.ensureQueryData(usersQueryOptions({ includeAdmin: false }));
+    return null
   },
   staticData: {
     routeFor: "admin",
@@ -34,65 +31,37 @@ export const Route = createFileRoute("/admin/user_management")({
 function RouteComponent() {
   logRender("Admin User Management Route");
 
-  const { data: users } = useSuspenseQuery(usersQueryOptions({ includeAdmin: false }))
-  const queryClient = useQueryClient()
+  const { mutateAsync: createUser, isPending } = createUserMutation()
 
   const onSubmit = async (fields: UserCreate) => {
-    const result = await createUser(fields)
-    if (!result.ok) {
-      showErrorToast("Error", result.error?.message)
-    } else {
-      await queryClient.invalidateQueries(usersQueryOptions({ includeAdmin: false }))
-      showSuccessToast("Success", "User created successfully")
-    }
+    createUser(fields)
   };
 
   return (
     <div className="flex flex-col gap-4">
       <div className="w-full flex justify-between">
-        <div>
-          <h1 className="text-4xl font-semibold">User Management</h1>
-          <p className="text-muted-foreground">
-            Manage data collectors and data managers
+        <div className="flex flex-col gap-1.5">
+          <h1 className="scroll-m-20 text-4xl font-bold tracking-tight text-balance">
+            User Management
+          </h1>
+          <p className="text-muted-foreground text-sm">
+            Manage data collectors and data managers.
           </p>
         </div>
-        <AddUserForm onSubmit={onSubmit} />
       </div>
-      <div className="flex flex-col gap-4">
-      <div>
-        <div className="w-72 flex items-center gap-2">
-          <SearchIcon className="text-muted-foreground" />
-          <Input placeholder="Search" />
+      <div className="flex flex-col gap-4 ">
+        <div className="space-y-4">
+          {/* <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+            Users
+          </h2> */}
+          <div className="flex justify-between">
+            <SearchBar />
+            <CreateUserDialog onSubmit={onSubmit} disabled={isPending} />
+          </div>
+          <Suspense fallback={<UsersTableSkeleton />}>
+            <UsersTableSuspense />
+          </Suspense>
         </div>
-      </div>
-        <h2 className="text-2xl font-semibold">Data Collectors</h2>
-        <Table className="flex-1 w-full h-full">
-          <TableHeader>
-              <TableRow>
-                <TableHead className="">Name</TableHead>
-                <TableHead className="">Email</TableHead>
-                <TableHead className="">Role</TableHead>
-                <TableHead className="">Status</TableHead>
-                <TableHead className="">Last Sign In</TableHead>
-                <TableHead className="">Actions</TableHead>
-              </TableRow>
-          </TableHeader>
-          <TableBody>
-            {users.map((user) => (
-              <TableRow 
-                key={user.id} 
-                className={`hover:bg-muted/50 ${user.role === "data_manager" && "bg-muted"}`}
-                >
-                <TableCell>{user.first_name} {user.last_name}</TableCell>
-                <TableCell>{user.email}</TableCell>
-                <TableCell>{ROLE_LABELS[user.role]}</TableCell>
-                <TableCell><StatusCell status={user.status} /></TableCell>
-                <TableCell><LastActiveCell lastActive={user.last_sign_in_at} /></TableCell>
-                <TableCell><UserActionsCell user={user} /></TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
       </div>
     </div>
   );
