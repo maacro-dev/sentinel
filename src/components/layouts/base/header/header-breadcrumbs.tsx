4e168cf3cc67695ed1899a150d271dbb0e24fba6
mawmao <meladryyy@gmail.com>
2025-07-10 14:34:11 +0800

    refactor: simplify HeaderBreadcrumbItem component by removing unnecessary props and restructuring rendering logic

--- src/components/layouts/base/header/header-breadcrumbs.tsx
+++ src/components/layouts/base/header/header-breadcrumbs.tsx
@@ -1,4 +1,4 @@
-import { BreadcrumbItem, BreadcrumbLink, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { BreadcrumbItem, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { memo } from "react";
 import { Fragment } from "react/jsx-runtime";
 
@@ -10,44 +10,18 @@ export type HeaderBreadcrumbItemProps = {
   isLast?: boolean;
 };
 
-export const HeaderBreadcrumbItem = memo(
-  ({
-    section,
-    title,
-    url,
-    isFirst = false,
-    isLast = false,
-  }: HeaderBreadcrumbItemProps) => {
-    if (isFirst && isLast) {
-      return (
-        <BreadcrumbItem>
-          <BreadcrumbPage>{title}</BreadcrumbPage>
-        </BreadcrumbItem>
-      );
-    }
-
-    return (
-      <Fragment>
-        {!isFirst && <BreadcrumbSeparator />}
-
-        {isFirst ? (
-          <BreadcrumbItem>
-            <BreadcrumbPage className="text-muted-foreground">
-              {section}
-            </BreadcrumbPage>
-          </BreadcrumbItem>
-        ) : (
-          <BreadcrumbItem>
-            {isLast ? (
-              <BreadcrumbPage>{title}</BreadcrumbPage>
-            ) : (
-              <BreadcrumbLink href={url}>{title}</BreadcrumbLink>
-            )}
-          </BreadcrumbItem>
-        )}
-
-        {!isLast && <BreadcrumbSeparator />}
-      </Fragment>
-    );
-  }
-);
+export const HeaderBreadcrumbItem = memo(({ section,title }: HeaderBreadcrumbItemProps) => {
+  return (
+    <Fragment>
+      <BreadcrumbItem>
+        <BreadcrumbPage className="text-muted-foreground">
+          {section}
+        </BreadcrumbPage>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbPage>{title}</BreadcrumbPage>
+      </BreadcrumbItem>
+    </Fragment>
+  );
+});
