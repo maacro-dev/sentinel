18b997a53d0a86defc4ed7cb525c2a169a009681
mawmao <meladryyy@gmail.com>
2025-07-10 14:33:21 +0800

    feat: enhance Toaster component with mobile positioning and duration settings

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,13 +1,16 @@
+import { isMobile } from "@/lib/utils";
 import { useTheme } from "next-themes";
 import { Toaster as Sonner, ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme();
-
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      expand
+      position={isMobile ? "bottom-center" : "bottom-right"}
+      duration={3000}
       style={
         {
           "--normal-bg": "var(--popover)",
