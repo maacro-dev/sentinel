be3775dddf7f6beca7897db445269cea0f5de127
mawmao <meladryyy@gmail.com>
2025-07-13 17:31:43 +0800

    feat: add meta tags and favicon link to head for improved SEO and user experience

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -20,6 +20,24 @@ export const Route = createRootRouteWithContext<RouterContext>()({
     await useAuthStore.getState().handleSession();
     logDebugOk("Session checked");
   },
+  head: () => ({
+    meta: [
+      {
+        name: "meta",
+        charSet: "UTF-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1.0",
+      }
+    ],
+    links: [
+      {
+        rel: 'icon',
+        href: 'favicon.ico',
+      },
+    ],
+  }),
   component: RootComponent,
 });
 
