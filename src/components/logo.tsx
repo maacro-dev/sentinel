7933519c7dc80e56fba75bdd7f7cd4f83117b507
mawmao <meladryyy@gmail.com>
2025-07-13 17:35:10 +0800

    refactor: replace logo component with SVG import for improved rendering and styling

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,14 +1,14 @@
-import { Leaf } from "lucide-react";
+import Logo from "@/assets/logo.svg?react";
 
 function HumayLogo() {
   return (
     <div className="flex items-center gap-2">
-      <div className="bg-green-600 text-white flex aspect-square size-8 items-center justify-center rounded-lg">
-        <Leaf className="size-4" />
-      </div>
-      <div className="grid flex-1 text-left text-sm leading-tight">
-        <span className="truncate font-medium">Humay</span>
-        <span className="truncate text-xs">Sentinel</span>
+      {/* <div className="bg-green-600 text-white flex aspect-square size-8 items-center justify-center rounded-lg"> */}
+        <Logo className="rounded-lg aspect-square size-12"/>
+      {/* </div> */}
+      <div className="flex flex-col mt-0.5">
+        <span className="truncate text-base font-medium leading-tight">Humay</span>
+        <span className="truncate text-[0.7rem] text-muted-foreground/60">Sentinel</span>
       </div>
     </div>
   );
