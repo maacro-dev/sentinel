272cc92dc4ea9dc77c9ce7d866bd198f48c8c7f0
mawmao <meladryyy@gmail.com>
2025-07-13 17:25:47 +0800

    chore: update supabase types generation to include public and analytics schemas

--- refresh_db.sh
+++ refresh_db.sh
@@ -1,1 +1,1 @@
-supabase db reset && supabase gen types typescript --local > ./src/app/supabase.types.ts
+supabase db reset && supabase gen types typescript --local --schema public,analytics > ./src/app/supabase.types.ts
