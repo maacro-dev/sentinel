84a8f29c8fdcd4ff4207627889e38b1404db8a6c
mawmao <meladryyy@gmail.com>
2025-07-13 17:27:14 +0800

    refactor: restructure Supabase client initialization to use dynamic imports and return the client instance

--- src/app/supabase.ts
+++ src/app/supabase.ts
@@ -1,24 +1,28 @@
-import { createClient } from "@supabase/supabase-js";
-import type { Database } from "@/app/supabase.types";
-import { IS_DEV } from "@/lib/utils";
+import { Database } from "./supabase.types";
 
-const supabaseDevUrl = import.meta.env.VITE_SUPABASE_DEV_URL;
-const supabaseDevKey = import.meta.env.VITE_SUPABASE_DEV_ANON_KEY;
+export async function getSupabase() {
 
-const supabaseProdUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseProdKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+  const { createClient } = await import("@supabase/supabase-js");
+  const { IS_DEV } = await import("@/lib/utils");
 
-const options = {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: false,
-    storage: localStorage,
-  },
+  const supabaseDevUrl = import.meta.env.VITE_SUPABASE_DEV_URL;
+  const supabaseDevKey = import.meta.env.VITE_SUPABASE_DEV_ANON_KEY;
+
+  const supabaseProdUrl = import.meta.env.VITE_SUPABASE_URL;
+  const supabaseProdKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+  const options = {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: false,
+      storage: localStorage,
+    },
+  };
+
+  const supabase = IS_DEV
+    ? createClient<Database>(supabaseDevUrl, supabaseDevKey, options)
+    : createClient<Database>(supabaseProdUrl, supabaseProdKey, options);
+
+  return supabase;
 }
-
-const supabase = IS_DEV ? 
-  createClient<Database>(supabaseDevUrl, supabaseDevKey, options) : 
-  createClient<Database>(supabaseProdUrl, supabaseProdKey, options);
-
-export { supabase };
