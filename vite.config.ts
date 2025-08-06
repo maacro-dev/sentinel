191d0f9e7cb39bdc8117309262851e90ca1effa4
mawmao <meladryyy@gmail.com>
2025-08-07 05:18:32 +0800

    fix: add exclude option for archive directory in tsconfig files and clean up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,40 +5,38 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import svgr from "vite-plugin-svgr";
 
-const ReactCompilerConfig = {
-  target: "19",
-  sources: (filename: string) => {
-    return filename.indexOf("src/core/lib") !== -1;
-  }
-};
-
 export default defineConfig({
+  base: "./",
   server: {
-    port: 3000
+    port: 3000,
   },
   preview: {
-    port: 3001
+    port: 3001,
   },
   plugins: [
-    svgr(),
-    tailwindcss(),
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
       routesDirectory: "./src/routes",
-      generatedRouteTree: "./src/core/tanstack/router/routeTree.gen.ts"
+      generatedRouteTree: "./src/core/tanstack/router/routeTree.gen.ts",
     }),
-
+    svgr(),
+    tailwindcss(),
     react({
       babel: {
-        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]]
-      }
+        plugins: [["babel-plugin-react-compiler", {
+          target: "19",
+            sources: (filename: string) => {
+              return filename.indexOf("src/core/lib") !== -1;
+            },
+          }]],
+      },
     }),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@features": path.resolve(__dirname, "./src/features"),
-    }
-  }
+    },
+  },
 });
