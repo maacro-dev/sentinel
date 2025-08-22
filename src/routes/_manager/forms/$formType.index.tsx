5129bec454b6cd217c5a626786c34c44cfa7f933
mawmao <meladryyy@gmail.com>
2025-08-23 03:14:50 +0800

    feat: use form detail sheet as a data access ui
    See #31

--- src/routes/_manager/forms/$formType.index.tsx
+++ src/routes/_manager/forms/$formType.index.tsx
@@ -3,6 +3,10 @@ import { createFileRoute } from '@tanstack/react-router'
 import { FormRouteType } from './-config';
 import { formDataOptions } from '@/features/forms/queries/options';
 import { FormDataTable } from '@/features/forms/components/FormDataTable';
+import { useState } from 'react';
+import { FormDetailSheet } from '@/features/forms/components/FormDetailSheet';
+import { useFormDetail } from '@/features/forms/hooks/useFormData';
+import { FormDataEntry } from '@/features/forms/schemas/formData';
 
 export const Route = createFileRoute('/_manager/forms/$formType/')({
   component: RouteComponent,
@@ -13,10 +17,26 @@ export const Route = createFileRoute('/_manager/forms/$formType/')({
 
 function RouteComponent() {
   const { formType } = Route.useParams();
+  const [detailOpen, setDetailOpen] = useState<boolean>(false)
+  const [selectedMfid, setSelectedMfid] = useState<string | null>(null);
 
-  return(
+  const { data } = useFormDetail(
+    formType as FormRouteType,
+    selectedMfid!,
+    { enabled: !!selectedMfid }
+  );
+
+  console.log(data)
+  return (
     <PageContainer>
-      <FormDataTable formType={formType as FormRouteType} />
+      <FormDataTable
+        formType={formType as FormRouteType}
+        onRowClick={(row) => {
+          setDetailOpen(true)
+          setSelectedMfid(row.mfid)
+        }}
+      />
+      <FormDetailSheet data={data as FormDataEntry} open={detailOpen} onOpenChange={setDetailOpen} />
     </PageContainer>
   )
 }
