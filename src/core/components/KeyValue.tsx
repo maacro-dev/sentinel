b16d16bef68066c479eb8ca4ee7ce8b3abdd00d6
mawmao <meladryyy@gmail.com>
2025-08-24 23:59:14 +0800

    feat: improve key-value component

--- src/core/components/KeyValue.tsx
+++ src/core/components/KeyValue.tsx
@@ -8,20 +8,22 @@ export type KVPair<Key = any, Value = any> = {
 
 export interface KVListProps extends React.ComponentProps<"div"> {
   itemsPerColumn?: number;
+  containerClassName?: string;
 }
 
-export const KVList = ({ className, itemsPerColumn = 6, children, ...props }: KVListProps) => {
+export const KVList = ({ containerClassName, className, itemsPerColumn = 6, children, ...props }: KVListProps) => {
   const childArray = React.Children.toArray(children);
 
+  // refactor: don't use loops ples
   const columns: React.ReactNode[][] = [];
   for (let i = 0; i < childArray.length; i += itemsPerColumn) {
     columns.push(childArray.slice(i, i + itemsPerColumn));
   }
 
   return (
-    <div className={cn(`flex`, className)} {...props}>
+    <div className={cn(`flex`, containerClassName)} {...props}>
       {columns.map((colItems, colIndex) => (
-        <dl key={colIndex} className="flex flex-col gap-4 flex-1">
+        <dl key={colIndex} className={cn("flex flex-col gap-4 flex-1", className)}>
           {colItems}
         </dl>
       ))}
@@ -31,13 +33,20 @@ export const KVList = ({ className, itemsPerColumn = 6, children, ...props }: KV
 
 interface KVItemProps extends React.ComponentProps<"div"> {
   pair: KVPair<string, string>;
+  variant?: "stacked" | "side";
 }
 
-export const KVItem = (props: KVItemProps) => {
+export const KVItem = ({ pair: { key, value }, variant = "stacked" }: KVItemProps) => {
   return (
-    <div className="flex flex-col gap-1.5 break-inside-avoid">
-      <dt className="font-semibold text-xs text-muted-foreground/60">{props.pair.key}</dt>
-      <dd className="text-xs text-foreground">{props.pair.value}</dd>
+    <div
+      className={cn(
+        "flex break-inside-avoid",
+        variant == "stacked" ? "flex-col gap-1.5" : "",
+        variant == "side" ? "w-full flex-row justify-between" : "",
+      )}
+    >
+      <dt className="font-semibold text-xs text-muted-foreground/60">{key}</dt>
+      <dd className="text-xs text-foreground truncate max-w-[180px]">{value}</dd>
     </div>
   )
 }
