fc5e971e51efe4d09ee75b55e3b64da76ef9bd43
mawmao <meladryyy@gmail.com>
2025-08-25 00:03:09 +0800

    chore: archive unused FormDetailSheet component

---
+++ archive/FormDetailSheet.tsx
@@ -0,0 +1,60 @@
+// import { Button } from "@/core/components/ui/button"
+// import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle } from "@/core/components/ui/sheet"
+// import { FormDataEntry } from "../schemas/formData"
+// import { KVList, KVItem } from "@/core/components/KeyValue"
+// import { flatten } from "@/core/utils/object"
+// import { Sanitizer } from "@/core/utils/sanitizer"
+// import { formKeyMappings } from "../mappings"
+
+// /**
+//  * UNUSED
+//  *
+//  * Supposed to be used for form detail
+//  * [ replaced with full-page detail ]
+//  */
+
+// interface FormDetailSheetProps {
+//   data: FormDataEntry | null
+//   open: boolean
+//   onOpenChange: (open: boolean) => void
+// }
+
+// export const FormDetailSheet = ({
+//   data,
+//   open,
+//   onOpenChange
+// }: FormDetailSheetProps) => {
+//   if (!data) return null
+
+//   return (
+//     <Sheet open={open} onOpenChange={onOpenChange}>
+//       <SheetContent forceMount>
+//         <SheetHeader>
+//           <SheetTitle>{Sanitizer.key(data.activity_type)}</SheetTitle>
+//           <SheetDescription>
+//             MFID - {Sanitizer.value(data.mfid)}
+//           </SheetDescription>
+//         </SheetHeader>
+//         <div className="grid flex-1 auto-rows-min gap-6 px-4">
+//           <KVList itemsPerColumn={8}>
+//             {flatten(data, "form_data").map(([key, value]) => (
+//               <KVItem
+//                 key={key}
+//                 pair={{
+//                   key: Sanitizer.key(key, formKeyMappings),
+//                   value: Sanitizer.value(value),
+//                 }}
+//               />
+//             ))}
+//           </KVList>
+//         </div>
+//         <SheetFooter>
+//           <Button type="submit">Verify</Button>
+//           <SheetClose asChild>
+//             <Button variant="outline">Close</Button>
+//           </SheetClose>
+//         </SheetFooter>
+//       </SheetContent>
+//     </Sheet>
+//   )
+// }
