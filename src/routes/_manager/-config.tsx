f00e097daf58c1102648262d0cd6e80958794d7f
mawmao <meladryyy@gmail.com>
2025-08-29 03:16:46 +0800

    feat: add new placeholder sidebar items

--- src/routes/_manager/-config.tsx
+++ src/routes/_manager/-config.tsx
@@ -3,6 +3,7 @@ import { analyticsGroupConfig } from "./_analytics/-config";
 import { overviewGroupConfig } from "./_overview/-config";
 import { createRouteConfig } from "@/core/tanstack/router/utils";
 import { buildSidebarConfig } from "@/core/utils/sidebar";
+import { FilePlus2, Import } from "lucide-react";
 
 export const managerGroupConfig = createRouteConfig("manager", {
   role: "data_manager",
@@ -11,6 +12,28 @@ export const managerGroupConfig = createRouteConfig("manager", {
     overviewGroupConfig,
     analyticsGroupConfig,
     formGroupConfig,
+
+    // TODO: these are still just placeholder items
+    createRouteConfig("data", {
+      role: "data_manager",
+      label: "Data",
+      children: [
+        createRouteConfig("import-export", {
+          role: "data_manager",
+          label: "Import",
+          path: "/unauthorized",
+          icon: Import,
+          disabled: true,
+        }),
+        createRouteConfig("generate-mfid", {
+          role: "data_manager",
+          label: "MFID Generation",
+          path: "/unauthorized",
+          icon: FilePlus2,
+          disabled: true,
+        }),
+      ]
+    }),
   ]
 });
 
