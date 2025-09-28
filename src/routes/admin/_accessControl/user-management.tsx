44d168a7551f3309640be63cce8ac6e7f1a5cc26
mawmao <meladryyy@gmail.com>
2025-09-29 02:48:30 +0800

    fix: add search validation for pagination

--- src/routes/admin/_accessControl/user-management.tsx
+++ src/routes/admin/_accessControl/user-management.tsx
@@ -4,12 +4,13 @@ import { INCLUDE_ADMIN } from "@/features/users/config";
 import { UsersTable } from "@/features/users/components/UsersTable";
 import { PageContainer } from "@/core/components/layout";
 import { createCrumbLoader } from "@/core/utils/breadcrumb";
+import { defaultPaginationSearchSchema } from "@/core/components/TablePagination";
 
 export const Route = createFileRoute("/admin/_accessControl/user-management")({
   component: RouteComponent,
+  validateSearch: defaultPaginationSearchSchema,
   loader: ({ context: { queryClient } }) => {
     queryClient.ensureQueryData(usersQueryOptions({ includeAdmin: INCLUDE_ADMIN }))
-
     return { breadcrumb: createCrumbLoader({ label: "User Management" }) }
   },
   head: () => ({
